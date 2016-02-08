dd9dcdfe160a0be84a024f4371803d338ede75ec
fiskurgit <jonathan@fiskur.eu>
2016-02-08 19:35:34 +0000

    better handling for resource ids

--- app/src/main/java/eu/fiskur/simpleviewpager/SimpleViewPager.java
+++ app/src/main/java/eu/fiskur/simpleviewpager/SimpleViewPager.java
@@ -18,205 +18,207 @@ import static eu.fiskur.simpleviewpager.R.drawable.circle;
 
 public class SimpleViewPager extends RelativeLayout {
 
-    private static final int MODE_URLS = 0;
-    private static final int MODE_DRAWABLES = 1;
-    private static final int MODE_IDS = 2;
-    private int mode = MODE_URLS;
+  private static final int MODE_URLS = 0;
+  private static final int MODE_DRAWABLES = 1;
+  private static final int MODE_IDS = 2;
+  private int mode = MODE_URLS;
 
-    private SimpleViewPagerAdapter adapter;
+  private SimpleViewPagerAdapter adapter;
 
-    private Context context;
-    private AViewPager viewPager;
+  private Context context;
+  private AViewPager viewPager;
 
-    private Drawable[] drawables = null;
-    private String[] imageUrls = null;
-    private int[] resourceIds = null;
+  private Drawable[] drawables = null;
+  private String[] imageUrls = null;
+  private int[] resourceIds = null;
 
-    //Circle indicators
-    private LinearLayout circleLayout;
-    private Drawable selectedCircle = null;
-    private Drawable unselectedCircle = null;
+  //Circle indicators
+  private LinearLayout circleLayout;
+  private Drawable selectedCircle = null;
+  private Drawable unselectedCircle = null;
 
-    private boolean forceSquare = false;
+  private boolean forceSquare = false;
 
-    public SimpleViewPager(Context context) {
-        super(context);
-        this.context = context;
-        setupViewPager();
+  public SimpleViewPager(Context context) {
+    super(context);
+    this.context = context;
+    setupViewPager();
+  }
+
+  public SimpleViewPager(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    this.context = context;
+
+    TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SimpleViewPager, 0, 0);
+
+    try {
+      forceSquare = a.getBoolean(R.styleable.SimpleViewPager_forceSquare, false);
+    } finally {
+      a.recycle();
     }
 
-    public SimpleViewPager(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        this.context = context;
+    setupViewPager();
+  }
 
-        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SimpleViewPager, 0, 0);
+  private void setupViewPager() {
+    viewPager = new AViewPager(context);
+    viewPager.setId(R.id.programmatic_viewpager);
+    addView(viewPager);
+  }
 
-        try {
-            forceSquare = a.getBoolean(R.styleable.SimpleViewPager_forceSquare, false);
-        } finally {
-            a.recycle();
-        }
+  //Drawables - not advised, but I'll leave it in.
+  //if you're using local images use the R.drawable resource instead and let Picasso do the loading
+  public void setup(Drawable[] drawables) {
+    mode = MODE_DRAWABLES;
+    this.drawables = drawables;
+    adapter = new SimpleViewPagerAdapter(context, drawables);
+    viewPager.setAdapter(adapter);
+  }
 
-        setupViewPager();
+  public void setup(Drawable[] drawables, ImageView.ScaleType scaleType) {
+    mode = MODE_DRAWABLES;
+    this.drawables = drawables;
+    adapter = new SimpleViewPagerAdapter(context, drawables);
+    if (scaleType != null) {
+      adapter.setScaleType(scaleType);
+    }
+    viewPager.setAdapter(adapter);
+  }
+
+  public void setup(Drawable[] drawables, ImageView.ScaleType scaleType, int indicatorColor,
+      int selectedIndicatorColor) {
+    mode = MODE_DRAWABLES;
+    this.drawables = drawables;
+    adapter = new SimpleViewPagerAdapter(context, drawables);
+    if (scaleType != null) {
+      adapter.setScaleType(scaleType);
+    }
+    viewPager.setAdapter(adapter);
+    setupIndicator(indicatorColor, selectedIndicatorColor);
+  }
+
+  //Urls for use with Picasso, Glide, or similar.
+  public void setup(String[] imageUrls, ImageURLLoader imageURLLoader) {
+    mode = MODE_URLS;
+    this.imageUrls = imageUrls;
+    adapter = new SimpleViewPagerAdapter(context, imageUrls, imageURLLoader);
+    viewPager.setAdapter(adapter);
+  }
+
+  public void setup(String[] imageUrls, int indicatorColor, int selectedIndicatorColor,
+      ImageURLLoader imageURLLoader) {
+    mode = MODE_URLS;
+    setup(imageUrls, imageURLLoader);
+    setupIndicator(indicatorColor, selectedIndicatorColor);
+  }
+
+  //IDs
+  public void setup(int[] resourceIds, ImageResourceLoader imageResourceLoader) {
+    mode = MODE_IDS;
+    this.resourceIds = resourceIds;
+    adapter = new SimpleViewPagerAdapter(context, resourceIds, imageResourceLoader);
+    viewPager.setAdapter(adapter);
+  }
+
+  public void setup(int[] resourceIds, ImageView.ScaleType scaleType, ImageResourceLoader imageResourceLoader) {
+    mode = MODE_IDS;
+    this.resourceIds = resourceIds;
+    adapter = new SimpleViewPagerAdapter(context, resourceIds, imageResourceLoader);
+    if (scaleType != null) {
+      adapter.setScaleType(scaleType);
+    }
+    viewPager.setAdapter(adapter);
+  }
+
+  public void setup(int[] resourceIds, ImageView.ScaleType scaleType, int indicatorColor, int selectedIndicatorColor, ImageResourceLoader imageResourceLoader) {
+    setup(resourceIds, scaleType, imageResourceLoader);
+    setupIndicator(indicatorColor, selectedIndicatorColor);
+  }
+
+  public void setupIndicator(int unselectedColor, int selectedColor) {
+    selectedCircle = ContextCompat.getDrawable(context, circle);
+    selectedCircle.setColorFilter(new PorterDuffColorFilter(selectedColor, PorterDuff.Mode.MULTIPLY));
+
+    unselectedCircle = ContextCompat.getDrawable(context, circle);
+    unselectedCircle.setColorFilter(
+        new PorterDuffColorFilter(unselectedColor, PorterDuff.Mode.MULTIPLY));
+
+    float scale = getResources().getDisplayMetrics().density;
+    int padding = (int) (5 * scale + 0.5f);
+
+    circleLayout = new LinearLayout(context);
+
+    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+    params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+    params.addRule(RelativeLayout.CENTER_HORIZONTAL);
+    params.bottomMargin = 75;
+    circleLayout.setLayoutParams(params);
+
+    addView(circleLayout);
+
+    for (int i = 0; i < adapter.getCount(); i++) {
+      ImageView circle = new ImageView(context);
+      circle.setImageDrawable(unselectedCircle);
+      circle.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+          ViewGroup.LayoutParams.WRAP_CONTENT));
+      circle.setAdjustViewBounds(true);
+      circle.setPadding(padding, 0, padding, 0);
+      circleLayout.addView(circle);
     }
 
-    private void setupViewPager(){
-        viewPager = new AViewPager(context);
-        viewPager.setId(R.id.programmatic_viewpager);
-        addView(viewPager);
+    setIndicator(0);
+
+    viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+      @Override
+      public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+      }
+
+      @Override public void onPageSelected(int position) {
+        setIndicator(position);
+      }
+
+      @Override public void onPageScrollStateChanged(int state) {
+
+      }
+    });
+  }
+
+  private void setIndicator(int index) {
+    int imageCount = adapter.getCount();
+
+    if (index >= imageCount) {
+      return;
     }
 
-    //Drawables
-    public void setup(Drawable[] drawables){
-        mode = MODE_DRAWABLES;
-        this.drawables = drawables;
-        adapter = new SimpleViewPagerAdapter(context, drawables);
-        viewPager.setAdapter(adapter);
+    ((ImageView) circleLayout.getChildAt(index)).setImageDrawable(selectedCircle);
+
+    if (index > 0) {
+      ((ImageView) circleLayout.getChildAt(index - 1)).setImageDrawable(unselectedCircle);
     }
 
-    public void setup(Drawable[] drawables, ImageView.ScaleType scaleType){
-        mode = MODE_DRAWABLES;
-        this.drawables = drawables;
-        adapter = new SimpleViewPagerAdapter(context, drawables);
-        if(scaleType != null) {
-            adapter.setScaleType(scaleType);
-        }
-        viewPager.setAdapter(adapter);
+    if (index < imageCount - 1) {
+      ((ImageView) circleLayout.getChildAt(index + 1)).setImageDrawable(unselectedCircle);
     }
+  }
 
-    public void setup(Drawable[] drawables, ImageView.ScaleType scaleType, int indicatorColor, int selectedIndicatorColor){
-        mode = MODE_DRAWABLES;
-        this.drawables = drawables;
-        adapter = new SimpleViewPagerAdapter(context, drawables);
-        if(scaleType != null) {
-            adapter.setScaleType(scaleType);
-        }
-        viewPager.setAdapter(adapter);
-        setupIndicator(indicatorColor, selectedIndicatorColor);
+  @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+    if (forceSquare) {
+      super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    } else {
+      int height = 0;
+      for (int i = 0; i < getChildCount(); i++) {
+        View child = getChildAt(i);
+        child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+        int h = child.getMeasuredHeight();
+        if (h > height) height = h;
+      }
+
+      heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
+      super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
-
-    //Urls
-    public void setup(String[] imageUrls, ImageLoader imageLoader) {
-        mode = MODE_URLS;
-        this.imageUrls = imageUrls;
-        adapter = new SimpleViewPagerAdapter(context, imageUrls, imageLoader);
-        viewPager.setAdapter(adapter);
-    }
-
-    public void setup(String[] imageUrls, int indicatorColor, int selectedIndicatorColor, ImageLoader imageLoader) {
-        mode = MODE_URLS;
-        setup(imageUrls, imageLoader);
-        setupIndicator(indicatorColor, selectedIndicatorColor);
-    }
-
-    //IDs
-    public void setup(int[] resourceIds){
-        mode = MODE_IDS;
-        this.resourceIds = resourceIds;
-        adapter = new SimpleViewPagerAdapter(context, resourceIds);
-        viewPager.setAdapter(adapter);
-    }
-
-    public void setup(int[] resourceIds, ImageView.ScaleType scaleType){
-        mode = MODE_IDS;
-        this.resourceIds = resourceIds;
-        adapter = new SimpleViewPagerAdapter(context, resourceIds);
-        if(scaleType != null) {
-            adapter.setScaleType(scaleType);
-        }
-        viewPager.setAdapter(adapter);
-    }
-
-    public void setup(int[] resourceIds, ImageView.ScaleType scaleType, int indicatorColor, int selectedIndicatorColor){
-        setup(resourceIds, scaleType);
-        setupIndicator(indicatorColor, selectedIndicatorColor);
-    }
-
-    public void setupIndicator(int unselectedColor, int selectedColor){
-        selectedCircle = ContextCompat.getDrawable(context, circle);
-        selectedCircle.setColorFilter(new PorterDuffColorFilter(selectedColor, PorterDuff.Mode.MULTIPLY));
-
-        unselectedCircle = ContextCompat.getDrawable(context, circle);
-        unselectedCircle.setColorFilter(new PorterDuffColorFilter(unselectedColor, PorterDuff.Mode.MULTIPLY));
-
-        float scale = getResources().getDisplayMetrics().density;
-        int padding = (int) (5 * scale + 0.5f);
-
-        circleLayout = new LinearLayout(context);
-
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-        params.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        params.bottomMargin = 75;
-        circleLayout.setLayoutParams(params);
-
-        addView(circleLayout);
-
-        for(int i = 0 ; i < adapter.getCount(); i++){
-            ImageView circle = new ImageView(context);
-            circle.setImageDrawable(unselectedCircle);
-            circle.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-            circle.setAdjustViewBounds(true);
-            circle.setPadding(padding, 0, padding, 0);
-            circleLayout.addView(circle);
-        }
-
-        setIndicator(0);
-
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                setIndicator(position);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-    }
-
-    private void setIndicator(int index){
-        int imageCount = adapter.getCount();
-
-        if(index >= imageCount) {
-            return;
-        }
-
-        ((ImageView) circleLayout.getChildAt(index)).setImageDrawable(selectedCircle);
-
-        if(index > 0){
-            ((ImageView) circleLayout.getChildAt(index - 1)).setImageDrawable(unselectedCircle);
-        }
-
-        if(index < imageCount - 1){
-            ((ImageView) circleLayout.getChildAt(index + 1)).setImageDrawable(unselectedCircle);
-        }
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
-        if(forceSquare){
-            super.onMeasure(widthMeasureSpec, widthMeasureSpec);
-        }else {
-            int height = 0;
-            for (int i = 0; i < getChildCount(); i++) {
-                View child = getChildAt(i);
-                child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
-                int h = child.getMeasuredHeight();
-                if (h > height) height = h;
-            }
-
-            heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
-
-            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        }
-    }
+  }
 }
 
